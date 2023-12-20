50612f7d7dde7e887bd7e4e7e5bcda6d6013424d
רותם קליר <rotem.kalir@post.runi.ac.il>
2023-12-20 14:51:58 +0200

    fixed

--- Coins.java
+++ Coins.java
@@ -4,9 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		int a= Integer.parseInt(args[0]);
-		int b= a/25;
-		int c= a%25;
-		System.out.println("Use "+b+" quarters and "+c+" cents");
+		int amount= Integer.parseInt(args[0]);
+		int quarters= amount/25;
+		int cents= amount%25;
+		System.out.println("Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -9,7 +9,7 @@ public class GenThree {
 		Random ran= new Random();
         int first = Integer.parseInt(args[0]);
 		int second = Integer.parseInt(args[1]);
-		int range = (second-first)+1;
+		int range = (second-first) + 1;
 		int rand1 = ran.nextInt(range)+first;
 		int rand2 = ran.nextInt(range)+first;
 		int rand3 = ran.nextInt(range)+first;
@@ -18,6 +18,6 @@ public class GenThree {
 		System.out.println(rand1);
 		System.out.println(rand2);
 		System.out.println(rand3);
-		System.out.println("The minimal generated number was "+min1);
+		System.out.println("The minimal generated number was " + min1);
 	}
 }
