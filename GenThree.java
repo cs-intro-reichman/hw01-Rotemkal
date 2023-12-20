93d819f9ff5a7332c5c9804f4da5959454edc0b4
רותם קליר <rotem.kalir@post.runi.ac.il>
2023-12-20 14:44:10 +0200

    fix

--- GenThree.java
+++ GenThree.java
@@ -1,8 +1,8 @@
+import java.util.Random;
 /*
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-import java.util.Random;
 public class GenThree {
 	public static void main(String[] args) {
 		import java.util.Random;
--- LinearEq.java
+++ LinearEq.java
@@ -10,7 +10,7 @@ public class LinearEq {
 		double b = Integer.parseInt(args[1]);
 		double c = Integer.parseInt(args[2]);
 		double d = c-b;
-		System.out.println(a+" * x "+b+" = "+c);
+		System.out.println(a+" * x + "+b+" = "+c);
 		System.out.println("x = "+d/a);
     }
 }
