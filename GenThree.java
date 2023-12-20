01809f7143b1bcf87c7d685e4fd1e1d2dcbb6490
רותם קליר <rotem.kalir@post.runi.ac.il>
2023-12-20 15:50:09 +0200

    HW1

--- GenThree.java
+++ GenThree.java
@@ -7,11 +7,14 @@ public class GenThree {
         int first = Integer.parseInt(args[0]);
 		int second = Integer.parseInt(args[1]);
 		int range = (second-first);
-		int rand1 = (int) (Math.random() * range) + first;
-		int rand2 = (int) (Math.random() * range) + first;
-		int rand3 = (int) (Math.random() * range) + first;
+
+		int rand1 = (int)(Math.random() * range) + first;
+		int rand2 = (int)(Math.random() * range) + first;
+		int rand3 = (int)(Math.random() * range) + first;
+
 		int min1= Math.min(rand1,rand2);
 		min1 = Math.min(min1,rand3);
+		
 		System.out.println(rand1);
 		System.out.println(rand2);
 		System.out.println(rand3);
